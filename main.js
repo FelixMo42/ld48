2799e1e8abe00ba045c23ce415305a1372e80ae0
FelixMo42 <felix@tacocat.com>
2020-10-02 16:21:32 -0700

    added ground

--- main.js
+++ main.js
@@ -23,15 +23,24 @@ const game = new Phaser.Game({
 let player;
 
 function preload() {
+    // load in all my beatifull art
     this.load.image("dog", "assets/dog.png")
+    this.load.image("ground", "assets/ground.png")
 }
 
 function create () {
-    player = this.physics.add.sprite(100, 100, 'dog')
+    player = this.physics.add.sprite(500, 100, 'dog')
     player.displayWidth = 200
     player.scaleY = player.scaleX
 
-    
+    // create a group for the platforms and ground
+    platforms = this.physics.add.staticGroup()
+
+    // make the player collide with the platforms
+    this.physics.add.collider(player, platforms)
+
+    // make the ground
+    platforms.create(0, window.innerHeight-50, 'ground').setScale(window.innerWidth).setOrigin(0).refreshBody()
 }
 
 function update() {   
