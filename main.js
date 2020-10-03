7030bb84f983d21b46488e0b47cd8775794ff9b6
FelixMo42 <felix@tacocat.com>
2020-10-03 11:37:22 -0700

    massive graphics update

--- main.js
+++ main.js
@@ -70,7 +70,7 @@ function spawnWave() {
 }
 
 function killEnemy(enemy) {
-    // decremnet the amount of enemeys on screen at the moment
+    // decrament the amount of enemeys on screen at the moment
     enemysLeft -= 1
 
     // incrament the kill counter
@@ -107,9 +107,21 @@ function addBoat() {
 function create () {
     this.cameras.main.backgroundColor.setTo(178,255,255)
 
-    this.add.image(0, window.innerHeight / 2 - 80, 'ocean').setOrigin(0)
+    water1 = this.physics.add.group()
+    water2 = this.physics.add.group()
+
+    let offset = 2216
+
+    // create the water behind the boat
+    water1.create(0, window.innerHeight / 2 - 80, 'ocean').setOrigin(0)
+    water1.create(0 + offset, window.innerHeight / 2 - 80, 'ocean').setOrigin(0)
+    
+    // add the boat
     addBoat.call(this)
-    this.add.image(-200, window.innerHeight / 2 + 80, 'ocean').setOrigin(0)
+
+    // create the water in front of the boat
+    water2.create(-200, window.innerHeight / 2 + 80, 'ocean').setOrigin(0)
+    water2.create(-200 + offset, window.innerHeight / 2 + 80, 'ocean').setOrigin(0)
 
     // spawn the player
     player = this.physics.add.sprite(500, 100, 'dog')
@@ -142,10 +154,26 @@ function create () {
 
 function loop(group) {
     for (let thing of group.getChildren()) {
-        if (thing.body.x + thing.body.width < -100) {
+        if (thing.body.x + thing.body.width < 0) {
             thing.setPosition(world_length + thing.body.x, thing.body.y)
         }
     }
+}
+
+function water_loop(w1, w2) {
+    if (w1.body.x + 2216 < 0) {
+        w1.setPosition(
+            w2.body.x + 2216,
+            w1.body.y
+        )
+    }
+}
+
+function waters_loop(waters) {
+    [w1, w2] = waters.getChildren()
+
+    water_loop(w1, w2)
+    water_loop(w2, w1)
 }
 
 function update() {
@@ -168,6 +196,8 @@ function update() {
 
     // update the velocity of the obsticles
     things.setVelocityX( -time / 100 - 200 )
+    water1.setVelocityX( -time / 100 - 200 )
+    water2.setVelocityX( -time / 100 - 250 )
     enemys.setVelocityX( -time / 100 - 300 )
 
     // make the player jump
@@ -187,6 +217,8 @@ function update() {
     // loop anything needs to be
     loop(things)
     loop(enemys)
+    waters_loop(water1)
+    waters_loop(water2)
 
     // see if you are dead
     if ( !gameOver && player.body.y > window.innerHeight ) {
