8440641da0a0e3985a7ba02bf187199f7e353a0e
FelixMo42 <felix@tacocat.com>
2020-10-02 17:41:40 -0700

    added looping enemys

--- main.js
+++ main.js
@@ -5,11 +5,7 @@ const game = new Phaser.Game({
 
     // set up some simple phisics
     physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 500 },
-            debug: false
-        }
+        default: 'arcade'
     },
 
     // register main game callbacks
@@ -20,28 +16,72 @@ const game = new Phaser.Game({
     }
 })
 
+const world_length = 2000;
+
 let player;
+let inputs;
+let things;
 
 function preload() {
     // load in all my beatifull art
     this.load.image("dog", "assets/dog.png")
     this.load.image("ground", "assets/ground.png")
+    this.load.image("blue", "assets/blue.png")
 }
 
 function create () {
     player = this.physics.add.sprite(500, 100, 'dog')
     player.displayWidth = 200
     player.scaleY = player.scaleX
+    player.setGravity(0, 500)
+
+
+    things = this.physics.add.group()
+
+    let enemy = things.create(700, 300, "blue").setScale(0.1)
+    enemy.setVelocityX(-200)
+
 
     // create a group for the platforms and ground
     platforms = this.physics.add.staticGroup()
 
+    // make the ground
+    platforms.create(0, window.innerHeight-50, 'ground').setScale(window.innerWidth).setOrigin(0).refreshBody()
+    platforms.create(350, window.innerHeight / 2, 'ground').setScale(300, 12).setOrigin(0).refreshBody()
+
     // make the player collide with the platforms
     this.physics.add.collider(player, platforms)
 
-    // make the ground
-    platforms.create(0, window.innerHeight-50, 'ground').setScale(window.innerWidth).setOrigin(0).refreshBody()
+    this.physics.add.collider(player, things, (player, thing) => {
+        thing.destroy()
+    });
+
+
+    // add bindings for the keys in the game
+    inputs = {
+        jump : this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W),
+
+        backup : this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A),
+        foward : this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D),
+    }
 }
 
 function update() {   
+    if (inputs.jump.isDown && player.body.touching.down) {
+        player.setVelocityY(-600)
+    }
+
+    if (inputs.foward.isDown) {
+        player.setVelocityX(160)
+    } else if (inputs.backup.isDown) {
+        player.setVelocityX(-160)
+    } else {
+        player.setVelocityX(0)
+    }
+
+    for (let thing of things.getChildren()) {
+        if (thing.body.x + thing.body.width < 0) {
+            thing.setPosition(world_length + thing.body.x, thing.body.y)
+        }
+    }
 }
